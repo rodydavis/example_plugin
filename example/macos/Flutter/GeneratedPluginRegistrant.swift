8f0a89f66a366d4a94999e4476990c9fbe931544
Rody Davis <rody.davis.jr@gmail.com>
2019-09-18 13:31:00 -0400

    Update GeneratedPluginRegistrant.swift

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import example_plugin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ExamplePlugin.register(with: registry.registrar(forPlugin: "ExamplePlugin"))
 }
